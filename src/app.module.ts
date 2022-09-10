0a878eb49cc4a75c5f5f8a3fd520982ba4e2725c
Matheus Afonso <matheusafonsouza@gmail.com>
2022-09-10 18:49:36 -0300

    fix: apollo dependency and typeorm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,22 @@ import { join } from 'path';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(),
-    GraphQLModule.forRoot({
-      autoSchemaFile: join(process.cwd(), 'src/schema.gql')
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'nest_api'
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      driver: ApolloDriver
     }),
     UserModule,
   ],
--- src/user/user.resolver.ts
+++ src/user/user.resolver.ts
@@ -1,4 +1,23 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { CreateUserInput } from './dto/create-user.input';
+import { UserService } from './user.service';
+import { User } from './user.entity';
 
-@Resolver()
-export class UserResolver {}
+@Resolver('User')
+export class UserResolver {
+    constructor (
+        private userService: UserService
+    ) {}
+
+    @Query(() => [User])
+    async findAll(): Promise<User[]> {
+        return this.userService.findAll();
+    }
+
+    @Mutation(() => User)
+    async create(
+        @Args('data') data: CreateUserInput
+    ): Promise<User> {
+        return this.userService.create(data);
+    }
+}
