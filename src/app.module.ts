7a22739515534316c03a5a8350ab9ffe25bc4cce
Matheus Afonso <matheusafonsouza@gmail.com>
2022-09-10 01:46:53 -0300

    feat: graphql configuration for application

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
+import { join } from 'path';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(),
+    GraphQLModule.forRoot({
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql')
+    }),
   ],
   controllers: [],
   providers: [],
